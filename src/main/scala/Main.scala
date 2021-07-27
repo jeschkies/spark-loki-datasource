bc7647e1cf5fb49029a3c2997348cde79aa04684
Karsten Jeschkies <k@jeschkies.xyz>
2021-07-27 13:42:01 +0200

    Ihnore project/project

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,7 +8,12 @@ object Main {
     val spark = SparkSession.builder.appName("CI Analysis").getOrCreate()
     import spark.implicits._
 
-    val lines = spark.readStream.textFile("*.md")
+    val lines = spark.readStream
+      .format("socket")
+      .option("host", "localhost")
+      .option("port", 9999)
+      .load()
+
     val words = lines.as[String].flatMap(_.split(" "))
     val wordCounts = words.groupBy("value").count()
 
