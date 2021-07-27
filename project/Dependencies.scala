74a6d7985bc897577eed32be9b0722dacb5dddd9
Karsten Jeschkies <k@jeschkies.xyz>
2021-07-27 17:19:12 +0200

    Define Loki receiver.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,10 +5,13 @@ object Dependencies {
   lazy val sparkVersion = "3.1.2"
 
   // Libraries
-  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion 
-  val sparkSql= "org.apache.spark" %% "spark-sql" % sparkVersion 
+  val scalaj = "org.scalaj" %% "scalaj-http" % "2.4.2"
+  val sparkStreaming = "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided"
+  val sparkSql= "org.apache.spark" %% "spark-sql" % sparkVersion % "provided" 
+
+  val ujson = "com.lihaoyi" %% "ujson" % "0.9.6"
 
   // Projects
   val backendDeps =
-    Seq(sparkStreaming, sparkSql)
+    Seq(scalaj, sparkStreaming, sparkSql, ujson)
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,40 @@
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
+import com.grafana.loki.LokiReceiver
 
 object Main {
 
   def main(args: Array[String]) {
-    val spark = SparkSession.builder.appName("CI Analysis").getOrCreate()
-    import spark.implicits._
+    //val spark = SparkSession.builder.appName("CI Analysis").getOrCreate()
+    //import spark.implicits._
 
+    // Create the context with a 1 second batch size
+    val sparkConf = new SparkConf().setAppName("CustomReceiver")
+    val ssc = new StreamingContext(sparkConf, Seconds(1))
+
+    /*
     val lines = spark.readStream
       .format("socket")
       .option("host", "localhost")
       .option("port", 9999)
       .load()
+      */
+    val endpoint = "https://logs-prod-us-central1.grafana.net"
+    val query = """{job="ci"}"""
+    val username = sparkConf.get("spark.driver.GRAFANA_USER")
+    val password = sparkConf.get("spark.driver.GRAFANA_TOKEN")
+    val lines = ssc.receiverStream(new LokiReceiver(endpoint, query, username, password))
 
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+    /*
     val words = lines.as[String].flatMap(_.split(" "))
     val wordCounts = words.groupBy("value").count()
 
@@ -23,6 +44,6 @@ object Main {
     .start()
 
     query.awaitTermination()
-    spark.stop()
+    spark.stop()*/
   }
 }
